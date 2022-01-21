c1e996d8340627203181a5c76cd666a77543f9e0
super-marios <wjckorea001@gmail.com>
2022-01-22 00:28:21 +0900

    added data graphql

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,12 +2,26 @@ import * as React from 'react';
 import Layout from '../components/layout';
 import { graphql } from 'gatsby';
 
-const BlogPage = () => {
+const BlogPage = ({ data }) => {
 	return (
 		<Layout pageTitle='My Blog Posts'>
-			<p>My cool posts will go in here</p>
+			<ul>
+				{data.allFile.nodes.map((node) => {
+					return <li key={node.name}>{node.name}</li>;
+				})}
+			</ul>
 		</Layout>
 	);
 };
+
+export const query = graphql`
+	query {
+		allFile {
+			nodes {
+				name
+			}
+		}
+	}
+`;
 
 export default BlogPage;
